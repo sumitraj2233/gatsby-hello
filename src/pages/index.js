42f424d293a93bdd280fe346bba723f009758a13
sumitraj2233 <sumitraj983541@gmail.com>
2023-05-18 16:04:19 +0530

    section chnages

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,63 +58,129 @@ import { Section } from "../components/Section";
 import { useState } from "react"
 
 
-export default function Home() {
-  const [activeSection, setActiveSection] = useState("product")
+// export default function Home() {
+//   const [activeSection, setActiveSection] = useState("product")
 
-  const handleSectionChange = (section) => {
-    setActiveSection(section)
+//   const handleSectionChange = (section) => {
+//     setActiveSection(section)
+//   }
+
+//   return (
+//     <div>
+//       <ul>
+//         <li>
+//           <Link to="/" onClick={() => handleSectionChange("product")}>
+//             Product
+//           </Link>
+//         </li>
+//         <li>
+//           <Link to="/" onClick={() => handleSectionChange("brand")}>
+//             Brand
+//           </Link>
+//         </li>
+//         <li>
+//           <Link to="/" onClick={() => handleSectionChange("host")}>
+//             Host
+//           </Link>
+//         </li>
+//         <li>
+//           <Link to="/" onClick={() => handleSectionChange("telo")}>
+//             Telo
+//           </Link>
+//         </li>
+//       </ul>
+//       <main>
+//         {activeSection === "product" && (
+//           <section id="product">
+//             {/* Content for the product section */}
+//             <Section />
+//           </section>
+//         )}
+//         {activeSection === "brand" && (
+//           <section id="brand">
+//             <Section />
+//             {/* Content for the brand section */}
+//           </section>
+//         )}
+//         {activeSection === "host" && (
+//           <section id="host">
+//             <Section />
+//             {/* Content for the host section */}
+//           </section>
+//         )}
+//         {activeSection === "telo" && (
+//           <section id="telo">
+//             <Section />
+//             {/* Content for the telo section */}
+//           </section>
+//         )}
+//       </main>
+//     </div>
+//   )
+// }
+
+import { useRef } from "react"
+// import { graphql } from "gatsby"
+
+export default function Home() {
+  // const { product, brand, host, telo } = data
+  const productRef = useRef(null)
+  const brandRef = useRef(null)
+  const hostRef = useRef(null)
+  const teloRef = useRef(null)
+
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" })
   }
 
   return (
     <div>
       <ul>
         <li>
-          <Link to="/" onClick={() => handleSectionChange("product")}>
-            Product
-          </Link>
+          <button onClick={() => scrollToSection(productRef)}>Product</button>
         </li>
         <li>
-          <Link to="/" onClick={() => handleSectionChange("brand")}>
-            Brand
-          </Link>
+          <button onClick={() => scrollToSection(brandRef)}>Brand</button>
         </li>
         <li>
-          <Link to="/" onClick={() => handleSectionChange("host")}>
-            Host
-          </Link>
+          <button onClick={() => scrollToSection(hostRef)}>Host</button>
         </li>
         <li>
-          <Link to="/" onClick={() => handleSectionChange("telo")}>
-            Telo
-          </Link>
+          <button onClick={() => scrollToSection(teloRef)}>Telo</button>
         </li>
       </ul>
       <main>
-        {activeSection === "product" && (
-          <section id="product">
-            {/* Content for the product section */}
-            product
-          </section>
-        )}
-        {activeSection === "brand" && (
-          <section id="brand">
-            brand
-            {/* Content for the brand section */}
-          </section>
-        )}
-        {activeSection === "host" && (
-          <section id="host">
-            host
-            {/* Content for the host section */}
-          </section>
-        )}
-        {activeSection === "telo" && (
-          <section id="telo">
-            telo
-            {/* Content for the telo section */}
-          </section>
-        )}
+        <section ref={productRef} key="productRef">
+          <h2>Product</h2>
+          <div >
+            <h3>title</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum fugiat, culpa tenetur, nisi ab modi vero ut aliquid itaque, tempore asperiores voluptate voluptates rem inventore magni sint molestiae sunt cupiditate!</p>
+          </div>
+
+        </section>
+        <section ref={brandRef}>
+          <h2>Brand</h2>
+          <div >
+            <h3>title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis alias, perspiciatis aut eveniet quasi tempora fugiat at esse placeat eos ut error, magnam sequi inventore et quos. Porro, voluptas esse!</p>
+          </div>
+        </section>
+        <section ref={hostRef}>
+          <h2>Host</h2>
+          <div >
+            <h3>title</h3>
+            <p>lorem</p>
+          </div>
+        </section>
+        <section ref={teloRef}>
+          <h2>Telo</h2>
+          <div >
+            <h3>title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores dolorem officiis non vero, libero sed cum minus. Quae vitae, obcaecati sequi delectus sint, perspiciatis ullam doloribus a voluptatibus amet nisi!</p>
+          </div>
+        </section>
       </main>
     </div>
   )
 }
+
